aaeba9311c81b22b9717d7e8a14722b63c081f65
David Bau <david.bau@gmail.com>
2022-08-19 22:26:27 +0000

    Handle www-urlformencoded and other robustness.

--- src/scripts/push.py
+++ src/scripts/push.py
@@ -1,21 +1,27 @@
-# Inspired by:
+# WSGI application to automaticallly deploy expo.baulab.info/2022-Fall/username
+# inspired by:
 # https://github.com/geopython/demo.pywps.org/blob/master/PyWPS/webhook-deploy.wsgi
 
 import hashlib
 import hmac
 import json
 import os
-import subprocess
+from subprocess import check_call, check_output
 import datetime
 import shutil
+import urllib.parse
 
 BASEDIR = '/srv/baulab/www'
 BASESITE = 'baulab.info'
 SECRETS = [b'LeCun'] # Rotate this secret for each semester
+SITES = ['expo'] # Sites where this is allowed
 
 def process_request(request, sitename='expo', event=None):
-    if not any(e == 'push' for e in request.get('events', []) + [event]):
-        return f'Ignored event {str(request["events"])}.'
+    if sitename not in SITES:
+        return f'Ignored push to {sitename}.'
+    events = request.get('events', []) + [event]
+    if not any(e == 'push' for e in events):
+        return f'Ignored event {events}.'
     owner_name = request['repository']['owner']['login']
     repo_name = request['repository']['name']
     if '..' in repo_name or '..' in owner_name:
@@ -24,90 +30,85 @@ def process_request(request, sitename='expo', event=None):
     if visibility != 'public':
         return f'Ignored push of nonpublic repository.'
     default_branch = request['repository']['default_branch']
-    clone_url = request['repository']['clone_url']
+    clone_url = request['repository']['clone_url'].strip()
     datedir = date_dir()
     # With this template, each github user only gets one website per semester.
-    website_dir = f'{BASEDIR}/{sitename}/{datedir}/{owner_name}'
+    subdir = f'{datedir}/{owner_name}'
+    website_dir = f'{BASEDIR}/{sitename}/{subdir}'
+    website_url = f'https://{sitename}.{BASESITE}/{subdir}'
     try:
         # If the website exists and is the right repository, update it.
+        if not os.path.isfile(f'{website_dir}/.git/HEAD'):
+            raise RuntimeError('New repo')
         os.chdir(website_dir)
-        with open(f'{website_dir}/.git/FETCH_HEAD') as f:
-            fetch_head_url = f.read().split()[-1]
-            if fetch_head_url != clone_url:
-                raise
-        subprocess.check_call(['git', 'fetch', '--depth=1'])
-        subprocess.check_call(['git', 'reset', '--hard', '@{u}'])
-    except:
+        repo_url = check_output(['git', 'remote', 'get-url', 'origin']).decode('utf-8').strip()
+        if clone_url != repo_url:
+            raise RuntimeError(f'Repo url changed {repo_url} to {clone_url}')
+        repo_branch = check_output(['git', 'branch', '--show-current']).decode('utf-8').strip()
+        if default_branch != repo_branch:
+            raise RuntimeError(f'Branch changed {repo_branch} to {default_branch}')
+        check_call(['git', 'fetch', '--depth=1'])
+        check_call(['git', 'reset', '--hard', '@{u}'])
+        return f'Fetched {default_branch} of {clone_url} to {website_url}.'
+    except Exception as e:
         # If there is some other problem, clean and recreate the website.
         shutil.rmtree(website_dir, ignore_errors=True)
         os.makedirs(website_dir, exist_ok=True)
         os.chdir(website_dir)
-        subprocess.call(['git', 'clone', '--depth=1', clone_url, '.'])
-        return f'Pushed to https://{sitename}.{BASESITE}/{datedir}/{owner_name}'
+        check_call(['git', 'clone', '--depth=1', clone_url, '.'])
+        return f'Cloned {default_branch} of {clone_url} to {website_url}. {e}.'
 
 def application(environ, start_response):
-    """WSGI application to update expo.baulab.info/2022-Fall/username/repo"""
-
-    error = 0
-    response = {
-        'status': '400 Bad Request',
-        'message': 'Could not update repository'
-    }
-
-    # read the request
-    length = int(environ.get('CONTENT_LENGTH', '0'))
-    signature = environ.get('HTTP_X_HUB_SIGNATURE_256', None)
-    event = environ.get('HTTP_X_GITHUB_EVENT', None)
-    sitename = environ.get('HTTP_HOST', 'expo').split('.')[0]
-    payload = environ['wsgi.input'].read(length)
+    """WSGI application to update expo.baulab.info/2022-Fall/username"""
 
     # parse the payload and check the signature
+    error = 0
     request = None
     secured = False
     message = 'Did nothing.'
+    status = '400 Bad request'
+    stamp = datetime.datetime.now().isoformat()
     try:
-        request = json.loads(payload)
+        # read the request
+        length = int(environ.get('CONTENT_LENGTH', '0'))
+        signature = environ.get('HTTP_X_HUB_SIGNATURE_256', None)
+        event = environ.get('HTTP_X_GITHUB_EVENT', None)
+        sitename = environ.get('HTTP_HOST', 'expo').split('.')[0]
+        ctype = environ.get('CONTENT_TYPE', 'application/json')
+        payload = environ['wsgi.input'].read(length)
+        # unwrap form-urlencoded payload
+        if ctype == 'application/x-www-form-urlencoded':
+            request = json.loads(urllib.parse.parse_qs(payload)[b'payload'][0])
+        else:
+            request = json.loads(payload)
+        # check the signature
         secured = any(is_valid_signature(s, signature, payload) for s in SECRETS)
-    except ValueError as e:
-        error = 1
-        request = {'error': str(e), 'payload': repr(payload) }
-        import traceback
-        message = traceback.format_exc()
-
-    # log the request
-    with open(f'{BASEDIR}/pushlog/log.txt', 'a') as f:
-        f.write(json.dumps(request, indent=1) + '\n')
-
-
-    if secured and not error:
-        try:
+        if not secured:
+            message = 'Webhook secret is wrong.'
+            status = '403 Forbidden'
+        else:
+            # process the action
             message = process_request(request, sitename=sitename, event=event)
-        except Exception as e:
-            error = 1
-            message = str(e)
+            status = '200 OK'
+    except Exception as e:
+        error = 1
+        import traceback
+        message = repr(traceback.format_exc())
+        with open(f'{BASEDIR}/pushlog/errors.txt', 'a') as f:
+            f.write(f'{stamp} ERROR {message} PAYLOAD {repr(payload)}\n')
+            for k, v in environ.items():
+                f.write(f'{stamp} {k} {v}\n')
 
-    if error:
-        response = {
-            'status': '500 Script error.',
-            'message': f'Could not process webhook {message}.'
-        }
-    elif not secured:
-        response = {
-            'status': '403 Forbidden',
-            'message': 'Webhook secret is wrong.'
-        }
-    else:
-        response = {
-            'status': '200 OK',
-            'secured': f'{secured}',
-            'message': message
-        }
+    # log the action
+    with open(f'{BASEDIR}/pushlog/log.txt', 'a') as f:
+        f.write(f'{stamp} {message}\n')
 
+    # send the response
+    response = {'status': status, 'message': message}
     output = json.dumps(response).encode('utf-8')
-
-    response_headers = [('Content-type', 'application/json'),
-                        ('Content-Length', str(len(output)))]
-    start_response(response['status'], response_headers)
+    start_response(response['status'], [
+        ('Content-type', 'application/json'),
+        ('Content-Length', str(len(output)))])
     return [output]
 
 def is_valid_signature(secret, signature, payload):
